9ebee862e616c147a22df61d28ddbff57fc7b59c
Layon Martins <layonmartins03@gmail.com>
2018-02-16 13:57:32 -0200

    create dominio: Faricante e Produto

---
+++ src/br/com/drogaria/domain/Fabricante.java
@@ -0,0 +1,23 @@
+package br.com.drogaria.domain;
+
+public class Fabricante {
+	private Long Codigo;
+	private String descricao;
+
+	public Long getCodigo() {
+		return Codigo;
+	}
+
+	public void setCodigo(Long codigo) {
+		Codigo = codigo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+}
